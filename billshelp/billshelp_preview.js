0e69fcde165e4849c9a85e7c8280b98365d48c23
Anna Samuel <52200507+samuelaai1@users.noreply.github.com>
2023-07-26 15:24:41 +0100

    Update billshelp_preview.js

--- billshelp/billshelp_preview.js
+++ billshelp/billshelp_preview.js
@@ -1,3 +1,5 @@
+// styling element so it looks like element when in version-test preview, or on version-live
+
 function(instance, properties) {
 
 	var input = $('<style>.custom-file-label {display: inline-block;background-color: #052E7C;color: white;border-radius: 5px;padding: 12px 16px;outline: none;white-space: nowrap;-webkit-user-select: none;cursor: pointer;font-weight: 600;font-size: 11pt;}</style><label for="file-upload" class="custom-file-label">Click to select files to upload</label>');
