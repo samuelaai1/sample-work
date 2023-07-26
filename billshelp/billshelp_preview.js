64cbf93dd542b2a808a29297775eaf77c63d3311
Anna Samuel <52200507+samuelaai1@users.noreply.github.com>
2023-07-26 14:08:35 +0100

    Create billshelp_preview.js

---
+++ billshelp/billshelp_preview.js
@@ -0,0 +1,7 @@
+function(instance, properties) {
+
+	var input = $('<style>.custom-file-label {display: inline-block;background-color: #052E7C;color: white;border-radius: 5px;padding: 12px 16px;outline: none;white-space: nowrap;-webkit-user-select: none;cursor: pointer;font-weight: 600;font-size: 11pt;}</style><label for="file-upload" class="custom-file-label">Click to select files to upload</label>');
+
+    instance.canvas.append(input);
+
+}/*style="color:white;background-color:#052E7C;border-radius:5px;font-weight:600;font-size:11pt">Click to select files to upload*/
